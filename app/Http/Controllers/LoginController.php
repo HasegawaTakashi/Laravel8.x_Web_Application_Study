205059a7fd4f8baddfaf78f459515c526119dc94
HasegawaTakashi <euphonium813@gmail.com>
2022-04-21 14:26:57 +0900

    add: アドレスとパスワードが一致すればhomeへリダイレクトする機能を追加しました。
    各機能説明
    $credentials = $request->only('email', 'password');
    単一の「配列」または引数の動的リストを受け入れる。
    
    Auth::attempt($credentials)
      attempt() データベーステーブルの中からユーザーを見つけるために使うもの
      - 引数にキー/値ペアの配列を受け取る
      - 今回はemailが一致するものを探す
      - passwordをハッシュ化してデータベース内のハッシュ化されたパスワードと一致するものを探す
      - 両方一致したらそのユーザーの新しい認証セッションを開始する
      - 認証が成功すればtrueを返し、失敗すればfalseを返す。
    
    $request->session()->regenerate();
      regenerate()
      セッションIDを再生成する
      セッション固定攻撃を防ぐため
    
    redirect()->intended(RouteServiceProvider::HOME);
      intended() 和訳: 意図された
      認証フィルターで引っかかる前にアクセスしようとしていたURLへリダイレクトしてくれるもの
      リダイレクトが不可能だった場合の移動先として、フォールバックURIを引数に指定する
    
      RouteServiceProvider::HOME
      App\Providers\RouteServiceProvider内に指定されている定数HOMEを呼び出す。
      public const HOME = '/home';

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,8 +10,22 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        return back()->withErrors([
+            'message' => 'メールアドレスまたはパスワードが正しくありません。',
+        ]);
     }
 }
